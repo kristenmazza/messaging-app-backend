88fad6fffa00c6b45c2c4398759ce64bcdee8202
kristenmazza <kbmazza@gmail.com>
2023-11-09 16:55:09 -0800

    Update allowed origins

--- src/setup/allowedOrigins.ts
+++ src/setup/allowedOrigins.ts
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-  'https://www.yoursite.com',
+  'https://km-messaging-app.fly.dev',
   'http://127.0.0.1:5500',
   'http://localhost:3000',
+  'http://localhost:5173',
 ];
 
 export default allowedOrigins;
