6726b4f7845b8da50aa5f6fce4b8c4f197f95729
kristenmazza <kbmazza@gmail.com>
2023-11-10 11:30:33 -0800

    Update error message

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -11,37 +11,42 @@ export const login_user = async (req: Request, res: Response) => {
       .status(400)
       .json({ message: 'Email and password are required.' });
 
-  const foundUser = await User.findOne({ email }).exec();
-  if (!foundUser) return res.sendStatus(401);
+  try {
+    const foundUser = await User.findOne({ email }).exec();
+    if (!foundUser) return res.sendStatus(401);
 
-  const match = await bcrypt.compare(password, foundUser.password);
+    const match = await bcrypt.compare(password, foundUser.password);
 
-  if (match) {
-    // Create JWTs
-    const accessToken = jwt.sign(
-      { email: foundUser.email },
-      process.env.ACCESS_TOKEN_SECRET as string,
-      { expiresIn: '30s' }
-    );
+    if (match) {
+      // Create JWTs
+      const accessToken = jwt.sign(
+        { email: foundUser.email },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        { expiresIn: '30s' }
+      );
 
-    const refreshToken = jwt.sign(
-      { email: foundUser.email },
-      process.env.REFRESH_TOKEN_SECRET as string,
-      { expiresIn: '1d' }
-    );
+      const refreshToken = jwt.sign(
+        { email: foundUser.email },
+        process.env.REFRESH_TOKEN_SECRET as string,
+        { expiresIn: '1d' }
+      );
 
-    // Saves refreshToken with current user
-    foundUser.refreshToken = refreshToken;
-    const result = await foundUser.save();
+      // Saves refreshToken with current user
+      foundUser.refreshToken = refreshToken;
+      const result = await foundUser.save();
 
-    res.cookie('jwt', refreshToken, {
-      httpOnly: true,
-      sameSite: 'none',
-      secure: true,
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-    res.json({ accessToken });
-  } else {
-    res.sendStatus(401);
+      res.cookie('jwt', refreshToken, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.json({ accessToken });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (err) {
+    const message = err instanceof Error ? err.message : String(err);
+    res.status(500).json({ message: message });
   }
 };
