985b9989f961ab58e0933a7a77c1a55619d5d903
kristenmazza <kbmazza@gmail.com>
2023-11-15 14:58:13 -0800

    Update how channel details are fetched

--- src/controllers/channelController.ts
+++ src/controllers/channelController.ts
@@ -3,28 +3,59 @@ import asyncHandler from 'express-async-handler';
 import { Channel } from '../models/channel';
 import mongoose from 'mongoose';
 
+// // Get channel details
+// export const channel_detail = asyncHandler(
+//   async (req: Request, res: Response): Promise<void> => {
+//     let channel;
+//     try {
+//       if (!mongoose.isValidObjectId(req.params.channelId)) {
+//         res.status(404).json({ message: 'Cannot find channel' });
+//       }
+
+//       channel = await Channel.findById(req.params.channelId)
+//         .populate('participants')
+//         .populate('latestMessage');
+
+//       if (!channel) {
+//         res.status(404).json({ message: 'Cannot find channel' });
+//       }
+//     } catch (err) {
+//       const message = err instanceof Error ? err.message : String(err);
+//       res.status(500).json({ message });
+//     }
+
+//     res.send(channel);
+//   }
+// );
+
 // Get channel details
 export const channel_detail = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
     let channel;
+    let response = { channelExists: false, channel: {} };
     try {
-      if (!mongoose.isValidObjectId(req.params.channelId)) {
-        res.status(404).json({ message: 'Cannot find channel' });
+      const user1 = req.query.user1;
+      const user2 = req.query.user2;
+
+      if (!user1 || !user2) {
+        res.status(400).json({ message: 'Invalid request' });
       }
 
-      channel = await Channel.findById(req.params.channelId)
-        .populate('participants')
-        .populate('latestMessage');
+      channel = await Channel.findOne({
+        participants: { $all: [user1, user2] },
+      }).populate('latestMessage');
 
-      if (!channel) {
-        res.status(404).json({ message: 'Cannot find channel' });
+      if (channel) {
+        response.channelExists = true;
+        response.channel = channel;
+        res.status(200).json(channel);
+      } else {
+        res.status(404).json(response);
       }
     } catch (err) {
       const message = err instanceof Error ? err.message : String(err);
       res.status(500).json({ message });
     }
-
-    res.send(channel);
   }
 );
 
