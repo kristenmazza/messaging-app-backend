1755b5915dae9ce8abe13cfbd128f556342efabb
kristenmazza <kbmazza@gmail.com>
2023-11-30 14:42:56 -0800

    Update allowed origins

--- src/setup/allowedOrigins.ts
+++ src/setup/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:3000',
   'http://localhost:5173',
+  'https://km-messaging-app.netlify.app',
 ];
 
 export default allowedOrigins;
